f2b391bde406781a24342ebececfcdd86da3a02e
Empyrealiv <2200795@sit.singaporetech.edu.sg>
2023-06-25 02:04:46 +0800

    Fixed bugs in FCFS

--- scheduling/fcfs.c
+++ scheduling/fcfs.c
@@ -26,38 +26,90 @@ void sortProcesses(Process processes[], int num_process) {
     }
 }
 
-void findtt(Process processes[], int num_process){
+void calculate(Process processes[], int num){
 
     int currentTime = 0;
 
-    for (int i = 0; i < num_process; i++)
+    for (int i = 0; i < num; i++)
     {
+        while (currentTime < processes[i].arrivalTime){
+            currentTime++;
+        }
         currentTime += processes[i].burstTime;
         processes[i].turnaroundTime = currentTime - processes[i].arrivalTime;
-    }
-    
-}
-
-void findwt(Process processes[], int num_process){
-    
-    for (int i = 0; i < num_process; i++)
-    {
         processes[i].waitingTime = processes[i].turnaroundTime - processes[i].burstTime;
     }
+    
+}
+
+void calculate_fcfs(Process processes[], int num){
+    
+    sortProcesses(processes, num);
+    calculate(processes, num);
 
 }
+
+// void calculate_fcfs(Process processes[], int num){
+
+//     int rt[num];
+//     int arrived[num];
+//     int completed[num];
+//     int flag = 0, current = -1, currentTime = 0, finishTime = 0, complete = 0, occupied = 0, responseTime = 0;
+
+//     for (int i = 0;i<num;i++){
+//         rt[i] = processes[i].burstTime;
+//         arrived[i] = 0;
+//         completed[i] = 0;
+//     }
+
+//     while(complete!=num){
+
+//         for (int j = 0;j<num;j++){
+//             if (processes[j].arrivalTime <= currentTime && rt[j] != 0){
+//                 arrived[j] = 1;
+//             }
+//         }
+
+//         for (int j = 0;j<num;j++){
+//             if (arrived[j] == 1 && completed[j] != 1 && occupied == 0){
+//                     flag = 1;
+//                     current = j;
+//                     occupied = 1;
+//                     responseTime = currentTime;
+//                 };
+//         }
+
+//         if (flag == 1){
+//             rt[current]--;
+//             currentTime++;
+//             if (rt[current] == 0){
+//                 complete++;
+//                 arrived[current] = 0;
+//                 completed[current] = 1;
+//                 finishTime = currentTime;
+//                 processes[current].turnaroundTime = finishTime - processes[current].arrivalTime;
+//                 processes[current].waitingTime = processes[current].turnaroundTime - processes[current].burstTime;
+//                 processes[current].responseTime = responseTime - processes[current].arrivalTime;
+//                 occupied = 0;
+//                 current = -1;
+//                 flag = 0;
+//             }
+//         }
+//         else{
+//             currentTime++;
+//         }
+//     }
+// }
 
 int main(){
 
     Process processes[3] = {
-        {1,0,2,24},
-        {2,0,0,3},
-        {3,0,1,3}
+        {1,0,0,2},
+        {2,0,4,7},
+        {3,0,5,6}
     };
 
-    sortProcesses(processes,3);
-    findtt(processes,3);
-    findwt(processes,3);
+    calculate_fcfs(processes, 3);
 
     for (int i=0;i<3;i++){
         printf("PID: %d\n", processes[i].processId);
